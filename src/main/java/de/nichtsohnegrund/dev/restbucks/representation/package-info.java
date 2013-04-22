a4bfa99b9fc35816fcf3d5ba638d64b55ecbb763
Peter Grund @ Bisnode <grund@INV42091.dom02.ads.bisnode.de>
2013-04-22 09:19:14 +0200

    undo

---
+++ src/main/java/de/nichtsohnegrund/dev/restbucks/representation/package-info.java
@@ -0,0 +1,7 @@
+@XmlSchema( 
+    namespace = Representation.RESTBUCKS_NAMESPACE, 
+    elementFormDefault = XmlNsForm.QUALIFIED) 
+package de.nichtsohnegrund.dev.restbucks.representation;
+ 
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
